9aca66945c2c89a6dbb8cd4061dc60c0f8f5f24e
Lucas <lucas_surmani93@hotmail.com>
2016-08-15 14:54:36 -0300

    hu3

---
+++ TrabalhoCG/src/trabalhocg/TrabalhoCG.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalhocg;
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author lucas
+ */
+public class TrabalhoCG {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        viewTrabalho view = new viewTrabalho();
+        view.setVisible(true);
+        
+        
+        
+    }
+    
+}
